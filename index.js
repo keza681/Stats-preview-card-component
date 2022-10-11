0bef8b161024d1646ef7caf6ba8049d5eacad93f
keza681 <lindakeza19@gmail.com>
2022-10-11 14:34:47 +0200

    Add some exercises

---
+++ index.js
@@ -0,0 +1,110 @@
+//integers
+//string
+//float  nombre avec virgule
+
+
+
+// var a="";
+// var x="";
+// var y=9;
+// var z= 9.70;
+
+
+
+
+
+
+
+// let num1=9;
+// let num2=8;
+
+// let sum=num1=num2;
+
+// let dif=num1 - num2;
+
+// let prod= num1 * num2;
+
+// let quot = num1 / num2;
+
+// console.log(dif)
+
+
+
+
+
+// let num1=9;
+// let num2=8;
+
+
+// if(num1 > num2) {
+//     console.log("num1 is greater than num2");
+// }else{
+//     console.log("num2 is greater than num1")
+// }
+
+
+
+// let numb1 = 14;
+// let numb2 = 7;
+
+// let sum = numb1 + numb2;
+// console.log(sum)
+
+// if(sum){
+
+// }
+
+
+
+// let remain = numb1 % numb2;
+// console.log(remain)
+
+// if(remain >= 1){
+//     console.log("The remainder exists")
+// }else{
+//     console.log("the remainder doesn't exist")
+// }
+
+
+
+// var x = prompt("enter a number");
+// var op = prompt("enter operator");
+// var z = prompt("enter second number");
+
+// var result;
+
+// if(op=="-"){
+//     result = x-z;
+
+// }else if(op=="*" || op=="x"){
+//     result = x*z;
+
+// }else if(op=="/"){
+//     result = x/z;
+
+// }else if (op=="+"){
+//     result = parseInt(x) + parseInt(z);
+// }
+
+// alert(`${x} ${op} ${z} = ${result}`);
+
+
+
+
+
+
+
+
+var u = prompt("enter username");
+var p = prompt("enter password");
+
+
+if(p=="password"){
+    alert("Welcome " + u);
+}else{
+    alert("Wrong password");
+}
+
+
+
+
