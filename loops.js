57b41facabafb72fc6dee0e864649c31c9ddb792
keza681 <lindakeza19@gmail.com>
2022-10-16 16:43:54 +0200

    Add another exercise

--- loops.js
+++ loops.js
@@ -14,16 +14,54 @@
 
 
 // for (let i = 5; i >= 0; i--){
-//     // console.log('Hello World' , i)
+//     // console.log('Hello World', i);
 //     if (i % 2 == 0) console.log(i);
 // }
 
 
 
 
-var age = 5;
 
-while (age < 10){
-    console.log("Your age is less than 10");
-    age++;
-}
+
+// while(condition){
+//     statement;
+//     incrementation;
+// }
+
+// var ...=...
+
+
+// var age = 5;
+
+// while (age < 10){
+//     console.log("Your age is less than 10");
+//     age++;
+// }
+// document.write("you are now over ten")
+
+
+
+
+
+// do{
+//     statement;
+//     incrementation;
+// } while(condtion);
+
+// var ...= ...
+
+
+// var x = 3;
+// // var x = 0;
+
+// do{
+//     document.write("<h2> Linda </h2>");
+//     // x--;
+//     x++;
+// }while(x<5);
+
+
+
+
+
+
