e93b2d916af5eee5bdaf3cfbe9474a42a0a9cd3b
keza681 <lindakeza19@gmail.com>
2022-10-16 14:43:28 +0200

    Add last exercises

--- index.js
+++ index.js
@@ -15,18 +15,18 @@
 
 
 
-let num1=9;
-let num2=8;
+// let num1=9;
+// let num2=8;
 
-let sum=num1=num2;
+// let sum=num1=num2;
 
-let dif=num1 - num2;
+// let dif=num1 - num2;
 
-let prod= num1 * num2;
+// let prod= num1 * num2;
 
-let quot = num1 / num2;
+// let quot = num1 / num2;
 
-console.log(dif)
+// console.log(dif)
 
 
 
@@ -124,5 +124,43 @@ console.log(dif)
 
 
 
+
+
+
+
+// function age (day, month, year){
+//     console.log("I was born on " + day + "/" + month + "/" + year)
+// }
+
+// age(12, 12, 1999)       J'AI REUSSI CAAAAAAAA OUUUUUUUUUH
+
+
+
+
+
+
+
+
+
+
+// To calculate age:
+// var year_born = prompt("Please enter your date of birth:", "Type here");
+// var d = new Date();
+// var n = d.getFullYear();
+// function getAge(birthYear){
+// 	var currentDate = new Date();
+//     var currentYear = currentDate.getFullYear();
+//     age = currentYear - birthYear;
+//     return age;
+// }
+// calculatedAge = getAge(year_born);
+// alert("Hello, " + "you are " + calculatedAge + " years old!");
+
+
+
+// function sum(x,y){
+//     console.log(x+y);
+// }
+// sum(3,4);
 
 
---
+++ loops.js
@@ -0,0 +1,29 @@
+// LOOPS
+// * For
+// * while
+// * do...while
+// * For... in
+// * For...of
+
+
+// (i=0 initial expression;
+// i<5 condition;
+// i++ incrementation){
+//     statement
+// }
+
+
+// for (let i = 5; i >= 0; i--){
+//     // console.log('Hello World' , i)
+//     if (i % 2 == 0) console.log(i);
+// }
+
+
+
+
+var age = 5;
+
+while (age < 10){
+    console.log("Your age is less than 10");
+    age++;
+}
